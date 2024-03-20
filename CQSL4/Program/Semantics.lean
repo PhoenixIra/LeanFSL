0a35eab7827e71803ccc0b90b545acf553bb2c16
Ira Fesefeldt <public@feuervogel.me>
2024-03-20 17:21:44 +0100

    finished states, progressing with semantics

--- CQSL4/Program/Semantics.lean
+++ CQSL4/Program/Semantics.lean
@@ -20,14 +20,14 @@ inductive Action where
 @[simp]
 noncomputable def skipSmallStepSemantics :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => ite_one_zero (C=terminated ∧ a = Action.deterministic ∧ s = s')
+  fun s a c s' => ite_one_zero (c = terminated ∧ a = Action.deterministic ∧ s = s')
 
 /-- valAssign succeeds if the expression is well-defined and the resulting state has changed.
     valAssign fails if the expression is not well-defined and the state remains unchanged. -/
 @[simp]
 noncomputable def valAssignSmallStepSemantics (v : Variable) (e : ValueExp Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | terminated => ite_one_zero (a = Action.deterministic ∧
     ∃ val, (e s.stack) = some val ∧ s.stack v ≠ none ∧ substituteStack s v val = s')
   | error => ite_one_zero (a = Action.deterministic ∧ s = s'
@@ -39,7 +39,7 @@ noncomputable def valAssignSmallStepSemantics (v : Variable) (e : ValueExp Varia
 @[simp]
 noncomputable def manipulateSmallStepSemantics (e_loc e_val : ValueExp Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | terminated => ite_one_zero (a = Action.deterministic ∧
       ∃ l, e_loc s.stack = some l ∧ s.heap l ≠ none ∧
       ∃ val, e_val s.stack = some val ∧ substituteHeap s l val = s')
@@ -55,7 +55,7 @@ noncomputable def manipulateSmallStepSemantics (e_loc e_val : ValueExp Variable)
 @[simp]
 noncomputable def lookupSmallStepSemantics (v : Variable) (e : ValueExp Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | terminated => ite_one_zero ( a = Action.deterministic ∧
       ∃ l, e s.stack = some l ∧ s.stack v ≠ none ∧
       ∃ val, s.heap l = some val ∧ substituteStack s v val = s' )
@@ -71,7 +71,7 @@ noncomputable def lookupSmallStepSemantics (v : Variable) (e : ValueExp Variable
 @[simp]
 noncomputable def compareAndSetSmallStepSemantics (v : Variable) (e_loc e_cmp e_val : ValueExp Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | terminated => ite_one_zero ( a = Action.deterministic ∧
       ∃ l, e_loc s.stack = some l ∧ ∃ cmp, e_cmp s.stack = some cmp
       ∧ ∃ new_val, e_val s.stack = some new_val
@@ -89,8 +89,8 @@ noncomputable def compareAndSetSmallStepSemantics (v : Variable) (e_loc e_cmp e_
 @[simp]
 noncomputable def allocateSmallStepSemantics (v : Variable) (n : ℕ) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' =>
-    ite_one_zero (∃ m, a = Action.allocation m ∧ C = terminated ∧ isNotAlloc s m n
+  fun s a c s' =>
+    ite_one_zero (∃ m, a = Action.allocation m ∧ c = terminated ∧ isNotAlloc s m n
       ∧ substituteStack (substituteHeap s m n) v m = s')
 
 /-- free succeeds if the expression is well-defined and the location is up to n positions allocated.
@@ -98,7 +98,7 @@ noncomputable def allocateSmallStepSemantics (v : Variable) (n : ℕ) :
 @[simp]
 noncomputable def freeSmallStepSemantics (e : ValueExp Variable) (n : ℕ) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | terminated => ite_one_zero (a = Action.deterministic ∧
     ∃ l, e s.stack = some l ∧ isAlloc s l n ∧ freeHeap s l n = s')
   | error => ite_one_zero (a = Action.deterministic ∧ s = s'
@@ -108,35 +108,35 @@ noncomputable def freeSmallStepSemantics (e : ValueExp Variable) (n : ℕ) :
 /-- probabilisticChoice succeeds if the expression is well-defined and picks one program with the given probability.
     probabilisticChoice fails if the expression is not well-defined. -/
 @[simp]
-noncomputable def probabilisticChoiceSmallStepSemantics (e : ProbExp Variable) (C₁ C₂ : Program Variable) :
+noncomputable def probabilisticChoiceSmallStepSemantics (e : ProbExp Variable) (c₁ c₂ : Program Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | error => ite_one_zero (a = Action.deterministic ∧ s = s' ∧ e s.stack = none)
-  | C => if a = Action.deterministic ∧ s = s' then if let some i := e s.stack then
-      if C₁ = C then i
-      else if C₂ = C then
+  | c => if a = Action.deterministic ∧ s = s' then if let some i := e s.stack then
+      if c₁ = c then i
+      else if c₂ = c then
         σ i else 0
     else 0 else 0
 
 /-- conditionalChoice succeeds if the expression is well-defined and picks the first if it is true and else the second.
     conditionalChoice fails if the epxression is not well-defined. -/
 @[simp]
-noncomputable def conditionalChoiceSmallStepSemantics (e : BoolExp Variable) (C₁ C₂ : Program Variable) :
+noncomputable def conditionalChoiceSmallStepSemantics (e : BoolExp Variable) (c₁ c₂ : Program Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C s' => match C with
+  fun s a c s' => match c with
   | error => ite_one_zero (a = Action.deterministic ∧ s = s' ∧ e s.stack = none)
-  | C => ite_one_zero (a = Action.deterministic ∧ s = s'
-    ∧ (e s.stack = some true ∧ C₁ = C ∨ e s.stack = some false ∧ C₂ = C))
+  | c => ite_one_zero (a = Action.deterministic ∧ s = s'
+    ∧ (e s.stack = some true ∧ c₁ = c ∨ e s.stack = some false ∧ c₂ = c ))
 
 /-- loopy Program that succeeds with terminated program if the loop condition is false,
     loops one more if the loop condition is true and errors if an expression is not well-defined-/
 @[simp]
-noncomputable def loopSmallStepSemantics (e : BoolExp Variable) (C : Program Variable) :
+noncomputable def loopSmallStepSemantics (e : BoolExp Variable) (c : Program Variable) :
     (State Variable) → Action → (Program Variable) → (State Variable) → I :=
-  fun s a C' s' => match C' with
+  fun s a c' s' => match c' with
   | error => ite_one_zero (a = Action.deterministic ∧ s = s' ∧ e s.stack = none)
   | terminated => ite_one_zero (a = Action.deterministic ∧ s = s' ∧ e s.stack = some false)
-  | C' => ite_one_zero (C' = sequential C (loop e C) ∧ s = s' ∧ e s.stack = some true )
+  | c' => ite_one_zero (c' = sequential c (loop e c) ∧ s = s' ∧ e s.stack = some true )
 
 noncomputable def programSmallStepSemantics :
     (Program Variable) → (State Variable) →
@@ -150,18 +150,18 @@ noncomputable def programSmallStepSemantics :
   | compareAndSet v e_loc e_cmp e_val => compareAndSetSmallStepSemantics v e_loc e_cmp e_val
   | allocate v n => allocateSmallStepSemantics v n
   | free e n => freeSmallStepSemantics e n
-  | probabilisticChoice e C₁ C₂ => probabilisticChoiceSmallStepSemantics e C₁ C₂
-  | conditionalChoice e C₁ C₂ => conditionalChoiceSmallStepSemantics e C₁ C₂
-  | loop e C => loopSmallStepSemantics e C
-  | sequential terminated C₂ => fun s a C s' => ite_one_zero (a = Action.deterministic ∧ s=s' ∧ C = C₂)
-  | sequential C₁ C₂ => fun s a C s' => match C with
-    | sequential C₁' C₂' => if C₂ = C₂' then (programSmallStepSemantics C₁ s a C₁' s') else 0
+  | probabilisticChoice e c₁ c₂ => probabilisticChoiceSmallStepSemantics e c₁ c₂
+  | conditionalChoice e c₁ c₂ => conditionalChoiceSmallStepSemantics e c₁ c₂
+  | loop e c => loopSmallStepSemantics e c
+  | sequential terminated c₂ => fun s a c s' => ite_one_zero (a = Action.deterministic ∧ s=s' ∧ c = c₂)
+  | sequential c₁ c₂ => fun s a c s' => match c with
+    | sequential c₁' c₂' => if c₂ = c₂' then (programSmallStepSemantics c₁ s a c₁' s') else 0
     | _ => 0
-  | concurrent terminated terminated => fun s a C s' => ite_one_zero (C = terminated ∧ a = Action.deterministic ∧ s = s')
-  | concurrent C₁ C₂ => fun s a C s' => match C with
-    | concurrent C₁' C₂' => match a with
-      | Action.concurrencyLeft a => if C₂ = C₂' then programSmallStepSemantics C₁ s a C₁' s' else 0
-      | Action.concurrencyRight a => if C₁ = C₁' then programSmallStepSemantics C₂ s a C₂' s' else 0
+  | concurrent terminated terminated => fun s a c s' => ite_one_zero (c = terminated ∧ a = Action.deterministic ∧ s = s')
+  | concurrent c₁ c₂ => fun s a c s' => match c with
+    | concurrent c₁' c₂' => match a with
+      | Action.concurrencyLeft a => if c₂ = c₂' then programSmallStepSemantics c₁ s a c₁' s' else 0
+      | Action.concurrencyRight a => if c₁ = c₁' then programSmallStepSemantics c₂ s a c₂' s' else 0
       | _ => 0
     | _ => 0
 
@@ -177,30 +177,86 @@ def enabledAction : (Program Variable) → (State Variable) → Set Action
   | compareAndSet _ _ _ _, _        => { Action.deterministic }
   | allocate _ n, s                 => { a | ∃ m, a = Action.allocation m ∧ isNotAlloc s m n }
   | free _ _, _                     => { Action.deterministic }
-  | sequential C₁ _, s              => if C₁ = terminated then { Action.deterministic } else enabledAction C₁ s
+  | sequential c₁ _, s              => if c₁ = terminated then { Action.deterministic } else enabledAction c₁ s
   | probabilisticChoice _ _ _, _    => { Action.deterministic }
   | conditionalChoice _ _ _, _      => { Action.deterministic }
   | loop _ _, _                     => { Action.deterministic }
-  | concurrent C₁ C₂, s             => { Action.concurrencyLeft a  | a ∈ enabledAction C₁ s }
-                                     ∪ { Action.concurrencyRight a | a ∈ enabledAction C₂ s }
+  | concurrent c₁ c₂, s             => { Action.concurrencyLeft a  | a ∈ enabledAction c₁ s }
+                                     ∪ { Action.concurrencyRight a | a ∈ enabledAction c₂ s }
 
 
 
-theorem zero_probability_of_not_enabledAction (a : Action) (h : ¬ a ∈ enabledAction C s):
-    ∀ C', ∀ s', programSmallStepSemantics C s a C' s' = 0 := by
-  intro C' s'
-  unfold programSmallStepSemantics
-  split
-  -- terminated
-  · simp only [Pi.zero_apply]
-  -- error
-  · simp only [Pi.zero_apply]
+theorem zero_probability_of_not_enabledAction (a : Action) (h : ¬ a ∈ enabledAction c s):
+    ∀ c', ∀ s', programSmallStepSemantics c s a c' s' = 0 := by
+  intro c' s'
+
+  -- split
+  induction c with
+  | terminated => unfold programSmallStepSemantics; simp only [Pi.zero_apply]
+  | error => unfold programSmallStepSemantics; simp only [Pi.zero_apply]
+  | skip =>
+    unfold programSmallStepSemantics skipSmallStepSemantics
+    rw[ite_one_zero_neg]; simp only [not_and_or]; exact Or.inr <| Or.inl h
+  | valAssign v e =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, valAssignSmallStepSemantics]
+    split
+    pick_goal 3; rfl
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | manipulate e e' =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, manipulateSmallStepSemantics]
+    split
+    pick_goal 3; rfl
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | lookup v e =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, lookupSmallStepSemantics]
+    split
+    pick_goal 3; rfl
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | compareAndSet v e_l e_v e_n =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, compareAndSetSmallStepSemantics]
+    split
+    pick_goal 3; rfl
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | allocate v n => sorry
+  | free v n =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, freeSmallStepSemantics]
+    split
+    pick_goal 3; rfl
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | sequential c₁ c₂ ih₁ ih₂ => sorry
+  | probabilisticChoice e c₁ c₂ _ _ =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, probabilisticChoiceSmallStepSemantics]
+    split
+    · rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h
+    · rw [if_neg]; intro h'; exact h h'.left
+  | conditionalChoice e c₁ c₂ _ _ =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, conditionalChoiceSmallStepSemantics]
+    split
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | loop e c ih =>
+    rw[enabledAction, Set.mem_singleton_iff] at h
+    simp only [programSmallStepSemantics, loopSmallStepSemantics]
+    split
+    all_goals (rw [ite_one_zero_neg]; simp only [not_and_or]; exact Or.inl h)
+  | concurrent c₁ c₂ ih₁ ih₂ => sorry
+
+
+
   -- allocation
-  pick_goal 6
-  · unfold allocateSmallStepSemantics
-    rw[ite_one_zero_neg]
-    simp only [not_exists, not_and]
-    intro n h_a h_terminating h_not_alloc
+  -- pick_goal 6
+  -- · unfold allocateSmallStepSemantics
+  --   rw[ite_one_zero_neg]
+  --   simp only [not_exists, not_and]
+  --   intro n h_a h_terminating h_not_alloc
+  --   rw [isNotAlloc_def] at h_not_alloc
+
 
 
 
--- CQSL4/Program/State.lean
+++ CQSL4/Program/State.lean
@@ -32,7 +32,7 @@ theorem substituteStack_stack {s : State Var} {v : Var} {q : ℚ} :
     exact (if_neg h_ne)
   · intro v' h_eq
     unfold substituteStack
-    rw[ite_eq_left_iff]
+    rw [ite_eq_left_iff]
     intro h_ne
     exfalso
     exact h_ne h_eq
@@ -44,17 +44,17 @@ theorem substituteStack_def {s s' : State Var} {v : Var} {q : ℚ} :
       ∧ (∀ v', v = v' → s'.stack v' = q) := by
   apply Iff.intro
   · intro h_substitute
-    rw[←h_substitute]
+    rw [←h_substitute]
     exact ⟨substituteStack_heap, substituteStack_stack⟩
-  · intro h_condition
+  · intro ⟨h_heap, h_remain, h_changed⟩
     unfold substituteStack
-    rw[State.mk.injEq]
+    rw [State.mk.injEq]
     apply And.intro
     · funext v'
       cases eq_or_ne v v' with
-      | inl h_eq => rw[if_pos h_eq]; exact (h_condition.right.right v' h_eq).symm
-      | inr h_ne => rw[if_neg h_ne]; exact (h_condition.right.left v' h_ne).symm
-    · exact h_condition.left.symm
+      | inl h_eq => rw [if_pos h_eq]; exact (h_changed v' h_eq).symm
+      | inr h_ne => rw [if_neg h_ne]; exact (h_remain v' h_ne).symm
+    · exact h_heap.symm
 
 
 noncomputable def substituteHeap
@@ -75,28 +75,28 @@ theorem substituteHeap_heap {s : State Var} {l : ℕ} {q : ℚ} :
     simp only
     exact (if_neg h_ne)
   · intro l' h_eq
-    rw[ite_eq_left_iff]
+    rw [ite_eq_left_iff]
     intro h_ne
     exfalso
     exact h_ne h_eq
 
-theorem substituteHeap_def {s s' : State Var} {v : Var} {q : ℚ} :
+theorem substituteHeap_def {s s' : State Var} {q : ℚ} :
     substituteHeap s l q = s' ↔ s'.stack = s.stack
       ∧ (∀ l', l ≠ l' → s'.heap l' = s.heap l')
       ∧ (∀ l', l = l' → s'.heap l' = q) := by
   apply Iff.intro
   · intro h_substitute
-    rw[←h_substitute]
+    rw [←h_substitute]
     exact ⟨substituteHeap_stack, substituteHeap_heap⟩
-  · intro h_condition
+  · intro ⟨h_stack, h_remain, h_changed⟩
     unfold substituteHeap
-    rw[State.mk.injEq]
+    rw [State.mk.injEq]
     apply And.intro
-    · exact h_condition.left.symm
+    · exact h_stack.symm
     · funext l'
       cases eq_or_ne l l' with
-      | inl h_eq => rw[if_pos h_eq]; exact (h_condition.right.right l' h_eq).symm
-      | inr h_ne => rw[if_neg h_ne]; exact (h_condition.right.left l' h_ne).symm
+      | inl h_eq => rw [if_pos h_eq]; exact (h_changed l' h_eq).symm
+      | inr h_ne => rw [if_neg h_ne]; exact (h_remain l' h_ne).symm
 
 
 
@@ -118,7 +118,7 @@ theorem removeLocationHeap_heap {s : State Var} {l : ℕ} :
     simp only
     exact (if_neg h_ne)
   . intro l' h_eq
-    rw[ite_eq_left_iff]
+    rw [ite_eq_left_iff]
     intro h_ne
     exfalso
     exact h_ne h_eq
@@ -129,27 +129,52 @@ lemma removeLocationHeap_def {s s' : State Var} {l : ℕ} :
       ∧ (∀ l', l = l' → s'.heap l' = none) := by
   apply Iff.intro
   · intro h_remove
-    rw[←h_remove]
+    rw [←h_remove]
     exact ⟨removeLocationHeap_stack, removeLocationHeap_heap⟩
-  · intro h_condition
+  · intro ⟨h_stack, h_remain, h_changed⟩
     unfold removeLocationHeap
-    rw[State.mk.injEq]
+    rw [State.mk.injEq]
     apply And.intro
-    · exact h_condition.left.symm
+    · exact h_stack.symm
     · funext l'
       cases eq_or_ne l l' with
       | inl h_eq =>
-        rw[if_pos h_eq]
-        exact (h_condition.right.right l' h_eq).symm
+        rw [if_pos h_eq]
+        exact (h_changed l' h_eq).symm
       | inr h_ne =>
-        rw[if_neg h_ne]
-        exact (h_condition.right.left l' h_ne).symm
+        rw [if_neg h_ne]
+        exact (h_remain l' h_ne).symm
 
 noncomputable def isNotAlloc
     (s : State Var) (l : ℕ) (n : ℕ): Prop :=
   match n with
   | Nat.zero => true
   | Nat.succ n => s.heap (l+n) = none ∧ isNotAlloc s l n
+
+theorem isNotAlloc_def (s : State Var) (l : ℕ) (n : ℕ) :
+    isNotAlloc s l n ↔ ∀ l', l ≤ l' → l' < l+n → s.heap l' = none := by
+  induction n with
+  | zero =>
+    unfold isNotAlloc; simp only [Nat.zero_eq, add_zero, true_iff]
+    intro l' h_le h_lt
+    exfalso
+    exact not_le_of_lt h_lt h_le
+  | succ n ih =>
+    unfold isNotAlloc
+    apply Iff.intro
+    · rintro ⟨h_none, h_alloc⟩ l' h_le h_lt
+      rw [Nat.add_succ, Nat.lt_succ] at h_lt
+      by_cases h : l' = l + n
+      · rw [h]; exact h_none
+      · exact ih.mp h_alloc l' h_le (lt_of_le_of_ne h_lt h)
+    · intro h
+      rw [Nat.add_succ] at h
+      apply And.intro
+      · exact h (l+n) le_self_add (Nat.lt_succ.mpr le_rfl)
+      · have : ∀ l', l ≤ l' → l' < l + n → s.heap l' = none := by
+          intro l' h_le h_lt
+          exact h l' h_le (Nat.lt_succ.mpr <| le_of_lt h_lt)
+        exact ih.mpr this
 
 noncomputable def allocateHeap
     (s : State Var) (l : ℕ) (n : ℕ) : State Var :=
@@ -163,69 +188,232 @@ theorem allocateHeap_stack {s : State Var} {l : ℕ} {n : ℕ} :
   | zero => simp only [allocateHeap]
   | succ n ih =>
     unfold allocateHeap
-    rw[substituteHeap_stack]
+    rw [substituteHeap_stack]
     exact ih
 
-theorem allocateHeap_heap {s s' : State Var} {l : ℕ} {n : ℕ} :
-    (allocateHeap s l n).heap = s'.heap →
-      (∀ l', (l' < l ∨ l+n ≤ l') → s'.heap l' = s.heap l')
-      ∧ (∀ l', l ≤ l' → l' < l+n → s'.heap l' = some 0) := by
-  intro h
-  rw[←h]; clear h
+theorem allocateHeap_heap {s : State Var} {l : ℕ} {n : ℕ} :
+      (∀ l', (l' < l ∨ l+n ≤ l') → (allocateHeap s l n).heap l' = s.heap l')
+      ∧ (∀ l', l ≤ l' → l' < l+n → (allocateHeap s l n).heap l' = some 0) := by
   apply And.intro
-  · intro l' h_l
-    induction n with
-    | zero => simp only [allocateHeap]
+  · induction n with
+    | zero => intro l' _; simp only [allocateHeap]
     | succ n ih =>
+      intro l' h_l
       unfold allocateHeap
       cases h_l with
       | inl h_lt =>
-        specialize ih (Or.inl h_lt)
-        rw[substituteHeap_heap.left l']
-        . exact ih
-        . apply ne_of_gt
-          sorry
-      | inr h_le => sorry
+        rw [substituteHeap_heap.left l']
+        · exact ih l' (Or.inl h_lt)
+        · intro h_eq; rw [← h_eq, add_lt_iff_neg_left] at h_lt; exact not_lt_zero' h_lt
+      | inr h_le =>
+        rw [substituteHeap_heap.left l']
+        · rw [Nat.add_succ, Nat.succ_le_iff] at h_le
+          apply le_of_lt at h_le
+          exact ih l' (Or.inr h_le)
+        · intro h_eq; rw [← h_eq, add_le_add_iff_left, Nat.succ_le, ← not_le] at h_le
+          exact h_le le_rfl
   · intro l' h_l₁ h_l₂
     induction n with
     | zero =>
-      rw[Nat.zero_eq, add_zero] at h_l₂
+      rw [Nat.zero_eq, add_zero] at h_l₂
       exfalso
       exact not_le_of_lt h_l₂ h_l₁
     | succ n ih =>
-      rw[Nat.add_succ] at h_l₂
+      rw [Nat.add_succ] at h_l₂
       cases lt_or_eq_of_le (Nat.le_of_lt_succ h_l₂) with
       | inl h_lt =>
         specialize ih h_lt
         unfold allocateHeap
-        rw[substituteHeap_heap.left l' (ne_of_lt h_lt).symm]
+        rw [substituteHeap_heap.left l' (ne_of_lt h_lt).symm]
         exact ih
       | inr h_eq =>
         unfold allocateHeap
         exact substituteHeap_heap.right l' h_eq.symm
 
+lemma allocateHeap_remain (s : State Var) (l : ℕ) (n : ℕ) :
+    ∀ l', (l' < l ∨ l+n ≤ l') → (allocateHeap s l n).heap l' = s.heap l' := by
+  induction n with
+  | zero => intro l' _; unfold allocateHeap; rfl
+  | succ n ih =>
+    intro l' h_l'
+    unfold allocateHeap
+    cases h_l' with
+    | inl h =>
+      have : l+n ≠ l' := by intro h_eq; rw [← h_eq, add_lt_iff_neg_left] at h; exact not_lt_zero' h
+      rw [substituteHeap_heap.left l' this]
+      exact ih l' (Or.inl h)
+    | inr h =>
+      rw [Nat.add_succ, Nat.succ_le] at h
+      cases eq_or_ne (l+n) l' with
+      | inl h_eq => exfalso; exact (ne_of_lt h) h_eq
+      | inr h_ne => rw [substituteHeap_heap.left l' h_ne]; exact ih l' (Or.inr <| le_of_lt h)
 
-lemma allocateHeap_def {s s' : State Var} {l : ℕ} {n : ℕ} :
+lemma allocateHeap_change (s : State Var) (l : ℕ) (n : ℕ) :
+    ∀ l', l ≤ l' → l' < l+n → (allocateHeap s l n).heap l' = some 0 := by
+  induction n with
+  | zero => intro l' h_le h_lt; rw [Nat.zero_eq, add_zero] at h_lt; exfalso; exact not_le_of_lt h_lt h_le
+  | succ n ih =>
+    intro l' h_le h_lt
+    unfold allocateHeap
+    rw [Nat.add_succ, Nat.lt_succ] at h_lt
+    by_cases h : l' = l + n
+    · exact substituteHeap_heap.right l' h.symm
+    · rw [substituteHeap_heap.left l' (Ne.symm h)]
+      exact ih l' h_le (lt_of_le_of_ne h_lt h)
+
+theorem allocateHeap_def {s s' : State Var} {l : ℕ} {n : ℕ} :
     allocateHeap s l n = s' ↔ s.stack = s'.stack
-      ∧ ∀ l, ((l' < l ∨ l+n ≤ l') → s.heap l' = s'.heap l')
-        ∧ (l ≤ l' → l' < l+n → s'.heap = some 0) := by
+      ∧ (∀ l', (l' < l ∨ l+n ≤ l') → s'.heap l' = s.heap l')
+      ∧ (∀ l', l ≤ l' → l' < l+n → s'.heap l' = some 0) := by
   apply Iff.intro
-  · intro h_alloc
-    unfold allocateHeap at h_alloc
-    rw[State.mk.injEq] at h_alloc
+  . intro h_alloc; rw [← h_alloc]
+    exact ⟨allocateHeap_stack.symm, allocateHeap_heap⟩
+  · intro ⟨h_stack, h_remain, h_changed⟩
+    rw [State.mk.injEq, ← h_stack]
+    apply And.intro allocateHeap_stack
+    funext l'
+    by_cases h : l' < l ∨ l+n ≤ l'
+    · rw [h_remain l' h]
+      exact allocateHeap_remain s l n l' h
+    · rw [not_or, not_lt, not_le] at h
+      obtain ⟨h_le, h_lt⟩ := h
+      rw [h_changed l' h_le h_lt]
+      exact allocateHeap_change s l n l' h_le h_lt
 
 
-
-noncomputable def isAlloc
-    (s : State Var) (l : ℕ) (n : ℕ) : Prop :=
+noncomputable def isAlloc (s : State Var) (l : ℕ) (n : ℕ) : Prop :=
   match n with
   | 0 => true
-  | Nat.succ n => ∃ v, s.heap (l+n) = some v ∧ isAlloc s l n
+  | Nat.succ n => (∃ v, s.heap (l+n) = some v) ∧ isAlloc s l n
+
+theorem isAlloc_def (s : State Var) (l : ℕ) (n : ℕ) :
+    isAlloc s l n ↔ ∀ l', l ≤ l' → l' < l+n → ∃ x, s.heap l' = some x := by
+  induction n with
+  | zero =>
+    unfold isAlloc; simp only [Nat.zero_eq, add_zero, true_iff]
+    intro l' h_le h_lt
+    exfalso
+    exact not_le_of_lt h_lt h_le
+  | succ n ih =>
+    unfold isAlloc
+    apply Iff.intro
+    · rintro ⟨⟨x, h_some⟩, h_alloc⟩ l' h_le h_lt
+      rw [Nat.add_succ, Nat.lt_succ] at h_lt
+      by_cases h : l' = l + n
+      · use x; rw [h]; exact h_some
+      · exact ih.mp h_alloc l' h_le (lt_of_le_of_ne h_lt h)
+    · intro h
+      rw [Nat.add_succ] at h
+      apply And.intro
+      · exact h (l+n) le_self_add (Nat.lt_succ.mpr le_rfl)
+      · have : ∀ l', l ≤ l' → l' < l + n → ∃ x, s.heap l' = some x := by
+          intro l' h_le h_lt
+          exact h l' h_le (Nat.lt_succ.mpr <| le_of_lt h_lt)
+        exact ih.mpr this
+
 
 noncomputable def freeHeap
     (s : State Var) (l : ℕ) (n : ℕ) : State Var :=
   match n with
   | 0 => s
   | Nat.succ n => removeLocationHeap (freeHeap s l n) (l+n)
+
+theorem freeHeap_stack {s : State Var} {l : ℕ} {n : ℕ} :
+    (freeHeap s l n).stack = s.stack := by
+  induction n with
+  | zero => simp only [freeHeap]
+  | succ n ih =>
+    unfold freeHeap
+    rw [removeLocationHeap_stack]
+    exact ih
+
+theorem freeHeap_heap {s : State Var} {l : ℕ} {n : ℕ} :
+      (∀ l', (l' < l ∨ l+n ≤ l') → (freeHeap s l n).heap l' = s.heap l')
+      ∧ (∀ l', l ≤ l' → l' < l+n → (freeHeap s l n).heap l' = none) := by
+  apply And.intro
+  · induction n with
+    | zero => intro l' _; simp only [freeHeap]
+    | succ n ih =>
+      intro l' h_l
+      unfold freeHeap
+      cases h_l with
+      | inl h_lt =>
+        rw [removeLocationHeap_heap.left l']
+        · exact ih l' (Or.inl h_lt)
+        · intro h_eq; rw [← h_eq, add_lt_iff_neg_left] at h_lt; exact not_lt_zero' h_lt
+      | inr h_le =>
+        rw [removeLocationHeap_heap.left l']
+        · rw [Nat.add_succ, Nat.succ_le_iff] at h_le
+          apply le_of_lt at h_le
+          exact ih l' (Or.inr h_le)
+        · intro h_eq; rw [← h_eq, add_le_add_iff_left, Nat.succ_le, ← not_le] at h_le
+          exact h_le le_rfl
+  · intro l' h_l₁ h_l₂
+    induction n with
+    | zero =>
+      rw [Nat.zero_eq, add_zero] at h_l₂
+      exfalso
+      exact not_le_of_lt h_l₂ h_l₁
+    | succ n ih =>
+      rw [Nat.add_succ] at h_l₂
+      cases lt_or_eq_of_le (Nat.le_of_lt_succ h_l₂) with
+      | inl h_lt =>
+        specialize ih h_lt
+        unfold freeHeap
+        rw [removeLocationHeap_heap.left l' (ne_of_lt h_lt).symm]
+        exact ih
+      | inr h_eq =>
+        unfold freeHeap
+        exact removeLocationHeap_heap.right l' h_eq.symm
+
+lemma freeHeap_remain (s : State Var) (l : ℕ) (n : ℕ) :
+    ∀ l', (l' < l ∨ l+n ≤ l') → (freeHeap s l n).heap l' = s.heap l' := by
+  induction n with
+  | zero => intro l' _; unfold freeHeap; rfl
+  | succ n ih =>
+    intro l' h_l'
+    unfold freeHeap
+    cases h_l' with
+    | inl h =>
+      have : l+n ≠ l' := by intro h_eq; rw [← h_eq, add_lt_iff_neg_left] at h; exact not_lt_zero' h
+      rw [removeLocationHeap_heap.left l' this]
+      exact ih l' (Or.inl h)
+    | inr h =>
+      rw [Nat.add_succ, Nat.succ_le] at h
+      cases eq_or_ne (l+n) l' with
+      | inl h_eq => exfalso; exact (ne_of_lt h) h_eq
+      | inr h_ne => rw [removeLocationHeap_heap.left l' h_ne]; exact ih l' (Or.inr <| le_of_lt h)
+
+lemma freeHeap_change (s : State Var) (l : ℕ) (n : ℕ) :
+    ∀ l', l ≤ l' → l' < l+n → (freeHeap s l n).heap l' = none := by
+  induction n with
+  | zero => intro l' h_le h_lt; rw [Nat.zero_eq, add_zero] at h_lt; exfalso; exact not_le_of_lt h_lt h_le
+  | succ n ih =>
+    intro l' h_le h_lt
+    unfold freeHeap
+    rw [Nat.add_succ, Nat.lt_succ] at h_lt
+    by_cases h : l' = l + n
+    · exact removeLocationHeap_heap.right l' h.symm
+    · rw [removeLocationHeap_heap.left l' (Ne.symm h)]
+      exact ih l' h_le (lt_of_le_of_ne h_lt h)
+
+theorem freeHeap_def {s s' : State Var} {l : ℕ} {n : ℕ} :
+    freeHeap s l n = s' ↔ s.stack = s'.stack
+      ∧ (∀ l', (l' < l ∨ l+n ≤ l') → s'.heap l' = s.heap l')
+      ∧ (∀ l', l ≤ l' → l' < l+n → s'.heap l' = none) := by
+  apply Iff.intro
+  . intro h_alloc; rw [← h_alloc]
+    exact ⟨freeHeap_stack.symm, freeHeap_heap⟩
+  · intro ⟨h_stack, h_remain, h_changed⟩
+    rw [State.mk.injEq, ← h_stack]
+    apply And.intro freeHeap_stack
+    funext l'
+    by_cases h : l' < l ∨ l+n ≤ l'
+    · rw [h_remain l' h]
+      exact freeHeap_remain s l n l' h
+    · rw [not_or, not_lt, not_le] at h
+      obtain ⟨h_le, h_lt⟩ := h
+      rw [h_changed l' h_le h_lt]
+      exact freeHeap_change s l n l' h_le h_lt
 
 end State
